ec5ecf69f28fbd980b4254aac82c698276432d2c
abdulsheikh759 <abdulsheikh759@gmail.com>
2025-12-03 17:09:57 +0530

    Set up React and tailwind css  project Implemented initial Header component UI

---
+++ src/components/Navbar/Header.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoIosSearch } from "react-icons/io";
+import { RiQuestionLine } from "react-icons/ri";
+import { IoMdSettings } from "react-icons/io";
+import { RiGeminiFill } from "react-icons/ri";
+import { CgMenuGridO } from "react-icons/cg";
+
+const Header = () => {
+    return (
+
+        <header className='bg-[#F8FAFD]'>
+            <nav className='flex items-center justify-between h-16 mx-auto max-w-[1300px] '>
+                <div className='flex items-center gap-10'>
+                    <div className='flex items-center gap-2'>
+                        <div className='p-3 rounded-full hover:bg-gray-100 cursor-pointer'>
+                            <RxHamburgerMenu size={"20px"} />
+                        </div>
+                        <img className='w-8' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmvBDVrWuIi8FBzBOXybue2OoHIgzNWo4AOA&s" alt="gmail logo" />
+                        <h1 className='text-2xl text-gray-600 font-medium'>Gmail</h1>
+                    </div>
+                </div>
+                <div className='md:block hidden w-[50%] mr-10'>
+                    <div className='flex items-center bg-[#E9EEF6] px-2 py-3 rounded-full'>
+                        <IoIosSearch size={"24px"} className='text-gray-700' />
+                        <input type="text"
+                            placeholder='Search Mail'
+                            className='rounded-full w-full bg-transparent outline-none px-2 text-black  placeholder:text-black/70 placeholder:font-semibold '
+                        />
+                    </div>
+                </div>
+                <div className='md:block hidden'>
+                    <div className='flex items-center gap-2'>
+                        <div className='p-3 rounded-full hover:bg-gray-100 cursor-pointer'>
+                            <RiQuestionLine size={"26px"} className='text-gray-600' />
+                        </div>
+                        <div className='p-3 rounded-full hover:bg-gray-100 cursor-pointer'>
+                            <IoMdSettings size={"24px"} className='text-gray-600' />
+                        </div>
+                        <div className='p-2 rounded-full hover:bg-blue-600 transition delay-150 duration-300 ease-in-out hover:rotate-180 cursor-pointer '>
+                            <RiGeminiFill size={"24px"} className='text-gray-600' />
+                        </div>
+                        <div className='p-3 rounded-full hover:bg-gray-100 cursor-pointer'>
+                            <CgMenuGridO size={"24px"} className='text-gray-600' />
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
